07559fddcab52e0f31ff34c802c49b757eec003b
Jadala Ramani <jadalaramani@gmail.com>
2025-12-31 14:07:42 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
-output "eks_cluster_name" {
+output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "runner_public_ip" {
-  value = aws_instance.gha_runner.public_ip
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.npm_app.repository_url
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
 }
